f8be095cc4bdfcbb99e6880468f6c0c44d6b056a
David Vezzani <david_vezzani@yahoo.com>
2018-03-10 19:35:17 -0700

    save state in memory

--- tigris-be/io.js
+++ tigris-be/io.js
@@ -1,7 +1,33 @@
+import _ from 'lodash';
+
 module.exports = function (io) {
 
 	console.log("should be listening for connections");
 	let nextUserId = 1000;
+  let gameBoard = null;
+  let gamePieceIdx = 0;
+  let movefrom = {};
+
+  const initGameBoard = (data) => {
+    gameBoard = JSON.parse(JSON.stringify(data));
+
+  }
+
+  const gameBoardDiff = (record, dirty) => {
+		// console.log(['gameBoardDiff', record, dirty]);
+
+    let diffGameBoard = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], ]
+    record.forEach((row, x) => {
+      row.forEach((col, y) => {
+        if (!_.isEqual(col, dirty[y][x])) {
+          // console.log(['asdf', ])
+          diffGameBoard[y][x] = dirty[y][x];
+        }
+      });
+    });
+    return diffGameBoard;
+  };
+  
 	io.on('connection', function(socket){
 		console.log('a user connected');
 
@@ -12,18 +38,64 @@ module.exports = function (io) {
 		socket.on('hello', function(data){
 			console.log(['hello received', data]);
 
-			socket.emit('hello', {greeting: 'Nice to see you again', userId: nextUserId+=1});
+			socket.emit('hello', {greeting: 'Nice to see you again', userId: nextUserId+=1, gameBoard});
+		});
+
+		socket.on('getData', function(data){
+			console.log(['getData command received']);//, JSON.stringify(data)]);
+
+			switch (data.cmd) {
+				case 'nextGamePieceId':
+          let gamePieceIds = [];
+          if (_.has(data.args, 'cnt')) {
+            gamePieceIds = _.range(data.args.cnt).map(()=>gamePieceIdx++);
+          } else {
+            gamePieceIds.push (gamePieceIdx++);
+          }
+					io.emit('getData', {userId: data.userId, cmd: 'nextGamePieceId', args: {gamePieceIds, ...data.args}, cb: data.cb||null});
+					break;
+			}
 		});
 
 		socket.on('sync', function(data){
 			console.log(['sync command received', JSON.stringify(data)]);
+      const domId = data.args.domId;
 
 			switch (data.cmd) {
+				case 'set':
+          if (gameBoard === null) { initGameBoard(data.args.gameBoard); };
+					// io.emit('sync', {userId: data.userId, cmd: 'movefrom', args: data.args});
+					break;
+
 				case 'movefrom':
+          if (!_.has(movefrom, data.userId)) {
+            movefrom[data.userId] = {};
+          }
+
+          movefrom[data.userId][domId] = data.args;
+          console.log(['asdf'])
+
 					io.emit('sync', {userId: data.userId, cmd: 'movefrom', args: data.args});
 					break;
+
 				case 'moveto':
-					io.emit('sync', {userId: data.userId, cmd: 'moveto', args: data.args});
+          let toX = data.args.to.x;
+          let toY = data.args.to.y;
+
+          if (_.has(movefrom[data.userId], domId)){
+            let data2 = movefrom[data.userId][domId]
+            console.log(['moveto:data2', data2])
+            let fromX = data2.from.x;
+            let fromY = data2.from.y;
+            gameBoard[fromY][fromX] = gameBoard[fromY][fromX].filter(gp => gp !== domId);
+            console.log([`moveto: removed game piece ${domId} from ${fromX}, ${fromY}`])
+          }
+
+          gameBoard[toY][toX].push (domId);
+          console.log([`moveto: added game piece ${domId} to ${toX}, ${toY}`])
+          
+          // let diff = gameBoardDiff(gameBoard, data.args.gameBoard);
+					io.emit('sync', {userId: data.userId, cmd: 'moveto', args: {gameBoard, ...data.args} });
 					break;
 			}
 		});
--- tigris-fe/src/components/GameBoard.vue
+++ tigris-fe/src/components/GameBoard.vue
@@ -13,15 +13,16 @@
 
 <script>
 import ioClient from 'socket.io-client'
+import _ from 'lodash';
 
 export default {
   name: 'GameBoard',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App', 
-			gameBoard: [[], [], [], [], [], [], [], [], [], [], [], ], 
-			offsetx: 82, 
-			offsety: 119, 
+			gameBoard: null, 
+			offsetX: 82, 
+			offsetY: 119, 
 			width: 40, 
 			height: 40, 
 			tileCount: 0,
@@ -35,60 +36,83 @@ export default {
 			$(gp).css({ position: 'absolute', left: x*40, top: y*40})
 			this.applyOffset($(gp), x, y);
 			// this.resetOffset($(gp), x, y);
+      let bp, ap = null;
+
+      bp = this.getBoardGamePieces().map(el => el[0].id);
 			this.gameBoard[y][x].push ( $(gp) );
+      ap = this.getBoardGamePieces().map(el => el[0].id);
+
+      if (bp.length === ap.length) {
+      }
 		}, 
 		applyOffset (gp, x, y) {
 			let stackOffset = this.gameBoard[y][x].length * this.stackOffset;
-			// console.log(['ui css, applyOffset', $(gp).css('top'), stackOffset]);
 			let left = parseInt($(gp).css('left').replace(/px/, ''));
 			let top = parseInt($(gp).css('top').replace(/px/, ''));
-			// console.log(['left/top, resetOffset', (left + stackOffset), (top + stackOffset)]);
 
 			$(gp).css({ left: (left + stackOffset) + 'px', top: (top + stackOffset) + 'px' });
 		}, 
     renderTileVisibility (row) {
       const self = this;
       // refresh tile visibility for the row
-      let zidx = 0;
+      let zIndex = 0;
       for (let col=15; col>=0; col-=1) {
         // for (let tileidx=self.gameBoard[row][col].length-1; tileidx>=0; tileidx-=1) {
         for (let tile of self.gameBoard[row][col]) {
           // let tile = self.gameBoard[row][col][tileidx];
-          $(tile).css('z-index', zidx);
-          zidx += 1;
+          $(tile).css('z-index', zIndex);
+          zIndex += 1;
         }
       }
     }, 
-    onDropped (gp, x, y, fromSelf=true) {
-      var self = this;
-			const coords = {x, y};
+    onDropped (gpJq, x, y, fromSelf=true, asgroup=false) {
+      const gp = gpJq[0];
+      const self = this;
 
       // determine where in the game board data that tile should go
       x = Math.floor(x / self.width)
       y = Math.floor(y / self.height)
+			const coords = {x, y};
 
 			self.sync.to = {x, y};
-
-			if (fromSelf === true) {
-        // console.log(['chk', {domId: `#${gp.id}`, from: self.sync.from, to: self.sync.to}]);
-        this.socket.emit('sync', {userId: self.sync.userId, cmd: 'moveto', args: {domId: `#${gp.id}`, to: coords} });
-			}
-
 
 			// place tile; apply offset if it was placed on a stack
 			$(gp).css({left: x * self.width, top: y * self.height})
 			self.applyOffset(gp, x, y);
 
 			// add to game board data grid
-			self.gameBoard[y][x].push (gp);
+			self.gameBoard[y][x].push (gpJq);
+
+			if (fromSelf === true) {
+        this.socket.emit('sync', {userId: self.sync.userId, cmd: 'moveto', args: {domId: `${gp.id}`, to: coords, gameBoard: self.serializeGameBoard()} });
+			}
 
       self.renderTileVisibility(y);
 
-			console.log(['tile was dropped; added in data table', gp.id, x, y]);
     },
-    onPickupStart (gp, x, y, fromSelf=true) {
-      var self = this;
-      console.log(['pick-up-start', gp, x, y, fromSelf]);
+    onAdd (gp, x, y, fromSelf=true) {
+    },
+    onMark (gp, x, y, fromSelf=true) {
+    },
+    onUnmark (gp, x, y, fromSelf=true) {
+    },
+    onRemove (gp, x, y, fromSelf=true, asGroup=false) {
+    },
+    initGameBoard () {
+      if (_.isNil(this.gameBoard)) {
+        this.gameBoard = [[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], ];
+      }
+      
+			// initialize gameBoard data
+			for (let idx=0; idx<352; idx+=1) {
+				let x = (idx % 16)
+				let y = Math.floor(idx / 16)
+				this.gameBoard[y][x] = [];
+			}
+    },
+    onPickupStart (gpJq, x, y, fromSelf=true, asGroup=false) {
+      const gp = gpJq[0];
+      const self = this;
 			const coords = {x, y};
 
 			if (fromSelf === true) {
@@ -100,33 +124,82 @@ export default {
 			self.sync.from = {x, y};
 
 			if (fromSelf === true) {
-        // console.log(['chk', {userId: self.sync.userId, domId: `#${gp.id}`, from: self.sync.from}]);
-        this.socket.emit('sync', {userId: self.sync.userId, cmd: 'movefrom', args: {domId: `#${gp.id}`, from: self.sync.from} });
+        this.socket.emit('sync', {userId: self.sync.userId, cmd: 'movefrom', args: {domId: `${gp.id}`, from: self.sync.from, gameBoard: self.serializeGameBoard()} });
 			}
 
 			self.gameBoard[y][x].pop();
 
-			console.log(['tile picked up; removed from data table', gp.id, x, y]);
+    },
+    renderGamePiece (gamePieceId, coords) {
+      const tiles = $( "#containment-wrapper" ).append(' <div id="game-piece-' + gamePieceId + '" class="game-piece game-piece-treasure draggable ui-widget-content"> <p>' + gamePieceId + '</p> </div> ');
+      const gp = $(tiles).children().last();
+      this.placeGamePiece (gp, ...coords);
+      return gp;
     },
     onInit () {
-      var self = this;
-			// initialize gameboard data
-			for (let idx=0; idx<176; idx+=1) {
-				let x = (idx % 16)
-				let y = Math.floor(idx / 16)
-				self.gameBoard[y][x] = [];
-			}
+      const self = this;
 
+      if (_.isNil(this.gameBoard)) { 
+        self.initGameBoard(); 
+      }
+
+      this.socket.emit('getData', {userId: this.sync.userId, cmd: 'nextGamePieceId', args: {cnt: 12}, cb: 'onInit02' });
+    },
+    serializeGameBoard () {
+      let gameBoardForServer = this.gameBoard.map((row, y) => {
+        return row.map((col, x) => {
+          return col.map((gp) => {
+            return gp[0].id;
+          });
+        });
+      });
+      return gameBoardForServer;
+    },
+    deserializeGameBoard (serializedGameBoard) {
+      let gameBoardForServer = serializedGameBoard.map((row, y) => {
+        return row.map((col, x) => {
+          return col.map((gp) => {
+            return $(gp[0].id);
+          });
+        });
+      });
+      return gameBoardForServer;
+    },
+    onInit02 (data) {
+      const self = this;
+      let gamePieceIds = data.nextGamePieceIds;
+      let gpIds = _.reverse(gamePieceIds);
+      
 			// place tiles on game board
 			for (let idx=0; idx<6; idx+=1) {
-				let chk = $( "#containment-wrapper" ).append(' <div id="game-piece-' + (self.tileCount++) + '" class="game-piece draggable ui-widget-content"> <p>' + (idx + 1) + '</p> </div> ');
-				self.placeGamePiece ($(chk).children().last(), 2, 2);
+        self.renderGamePiece (gpIds.pop(), [2, 2]);
 			}
+      self.renderTileVisibility(2);
 
-      let chk = $( "#containment-wrapper" ).append(' <div id="game-piece-' + (self.tileCount++) + '" class="game-piece game-piece-monument draggable ui-widget-content"> <p>M</p> </div> ');
-      self.placeGamePiece ($(chk).children().last(), 4, 4);
-			
+      [ 
+        [1, 1], 
+        [5, 2], 
+        [10, 0], 
+        [15, 1], 
+        [1, 7], 
+      ].forEach((coords) => {
+        self.renderGamePiece (gpIds.pop(), coords);
+        self.renderTileVisibility(coords[1]);
+      });
+
+      let monumentTile = $( "#containment-wrapper" ).append(' <div id="game-piece-' + gpIds.pop() + '" class="game-piece game-piece-monument draggable ui-widget-content"> <p>M</p> </div> ');
+      self.placeGamePiece ($(monumentTile).children().last(), 4, 4);
+      self.renderTileVisibility(4);
 	
+      const gameBoardForServer = self.serializeGameBoard();
+
+      console.log(['gameBoardForServer', gameBoardForServer]);
+      this.socket.emit('sync', {userId: this.sync.userId, cmd: 'set', args: {gameBoard: gameBoardForServer} });
+
+      self.setGamePieceEvents()
+    },
+    setGamePieceEvents () {
+      const self = this;
 			// define draggable behavior
 			$( ".game-piece" ).draggable({ 
 				containment: "#containment-wrapper", 
@@ -136,29 +209,112 @@ export default {
 				grid: [ 40, 40 ], 
 				start: function( event, ui ) {
           const gp = ui.helper[0];
-          self.onPickupStart (gp, gp.offsetLeft, gp.offsetTop);
+          self.onPickupStart (ui.helper, gp.offsetLeft, gp.offsetTop);
 				}
 			});
 			
 			$( "#playing-field" ).droppable({
 				drop: function( event, ui ) {
-          const gp = ui.helper[0];
-          self.onDropped (gp, event.clientX - self.offsetx, event.clientY - self.offsety);
+          // const gp = ui.helper[0];
+          self.onDropped (ui.helper, event.clientX - self.offsetX, event.clientY - self.offsetY);
 				}
 			});
     },
+    getBoardGamePieces (gameBoard = this.gameBoard){
+      let report = gameBoard.map((row) => {
+        let res, res2 = null;
+        res2 = [];
+        res = row.filter(entry => entry.length > 0)
+
+        if (!_.isNil(res) && res.length > 0) {
+          res2 = (res || []).reduce((collection, arr) => collection.concat(arr));
+        }
+        return res2
+      })
+      report = report.filter(entry => entry.length > 0)
+
+      if (!_.isNil(report) && report.length > 0) {
+        report = report.reduce((collection, arr) => collection.concat(arr));
+      }
+
+      return report;
+    },
+    syncGameBoard (gameBoard, newGamePieces){
+      
+      this.initGameBoard();
+
+      gameBoard.forEach((row, y) => {
+        row.forEach((col, x) => {
+          col.forEach((gpId) => {
+            if (_.includes(newGamePieces, gpId)) {
+              console.log(['gpId', gpId]);
+              let gpIdx = gpId.match(/^game-piece-(.+)$/)[1];
+              this.renderGamePiece (gpIdx, [x, y]);
+              this.renderTileVisibility(y);
+            }
+          });
+        });
+      });
+    },
     hello (data){
-			console.log(['hello received', data]);
 			if (this.sync.userId === null) {
 				this.sync.userId = data.userId;
 			}
+
+      if (this.gameBoard === null) {
+        if (_.isNil(data.gameBoard)) {
+          this.onInit();
+        } else {
+          console.log(['data.gameBoard', data.gameBoard])
+          // TODO: serialize as needed; more efficient?
+          // TODO: create tiles missing on client that were created by someone else
+
+          // current player gps
+          let playerGps = null;
+          if (!_.isNil(this.gameBoard)) {
+            playerGps = this.getBoardGamePieces();
+          }
+          
+          // server gps
+          let serverGps = null;
+          if (!_.isNil(data.gameBoard)) {
+            serverGps = this.getBoardGamePieces(data.gameBoard);
+            // this.gameBoard = this.deserializeGameBoard(data.gameBoard);
+          }
+
+          let newGps = serverGps.filter(gpId => !_.includes(playerGps, gpId))
+          console.log(['newGps', playerGps, serverGps, newGps, data.gameBoard]);
+
+          this.syncGameBoard(data.gameBoard, newGps);
+
+          // this.socket.emit('getData', {userId: this.sync.userId, cmd: 'nextGamePieceId', args: {cnt: 3}, cb: 'blah' });
+          // if (_.isNil(playerGps) && !_.isNil(serverGps)) {
+          //   serverGps.forEach((gpId, idx) => {
+          //     let gpIdx = gpId.match(/^game-piece-(.+)$/)[1];
+          //     let gp = this.renderGamePiece (gpIdx, [((idx <= 5) ? 1 : 2), 10]);
+          //   })
+          //   this.renderTileVisibility(10);
+          // }
+
+          this.setGamePieceEvents()
+        }
+      }
+		},
+    blah () {
+		}, 
+    getData (data){
+			switch (data.cmd) {
+				case 'nextGamePieceId':
+					let cb = data.cb;
+          let nextGamePieceIds = data.args.gamePieceIds;
+          this[cb]({nextGamePieceIds});
+					break;
+			}
 		}, 
     syncGame (data){
-			let domId, toX, toY, fromX, fromY, gp = null;
+			let domId, toX, toY, fromX, fromY, gameBoard, gp = null;
 
-			console.log(['sync command received', JSON.stringify(data)]);
 			if (data.userId === this.sync.userId) {
-				console.log(['event was triggered by this user; events already applied', data.userId]);
 				return;
 			}
 			switch (data.cmd) {
@@ -166,11 +322,11 @@ export default {
 					toX = data.args.to.x;
 					toY = data.args.to.y;
 					domId = data.args.domId;
+					gameBoard = data.args.gameBoard;
 					gp = $(domId)
 
 					// TODO: need to have another way to deal with conflicts when two or more players
 					// 		are attempting to move the same piece at the same time
-					console.log(['chk, dropped', gp, toX, toY, false]);
           this.onDropped (gp, toX, toY, false);
 					break;
 
@@ -180,22 +336,19 @@ export default {
 					domId = data.args.domId;
 					gp = $(domId)
 
-					console.log(['chk, pick-up-start', gp, fromX, fromY, false]);
           this.onPickupStart (gp, fromX, fromY, false);
 					break;
 			}
 		},
 	}, 
 	mounted () {
-		var self = this;
-
-    self.onInit();
+		const self = this;
 
 		this.socket = ioClient.connect("http://tigris.reliacode.com:8082");
-		console.log(["should have connected to io", this.socket]);
 
 		this.socket.on('hello', self.hello);
 		this.socket.on('sync', self.syncGame);
+		this.socket.on('getData', self.getData);
     this.socket.emit('hello', { greeting: 'hi' });
 	}
 }
@@ -223,14 +376,13 @@ body {
 #playing-field, 
 #containment-wrapper { 
 	width: 645px;
-	height: 440px;
+	height: 640px;
 }
 #playing-field {
 	position: relative;
 	left: 72px;
   top: 57px;	
-	width: 641px;
-	height: 440px;
+  /* border: 2px solid blue; */
 }
 h3 { clear: left; }
 .spacer-x {
@@ -243,6 +395,9 @@ h3 { clear: left; }
 	height: 5px;
 	width: 38px;
 	position: relative;
+}
+.game-piece-treasure {
+  background-color: yellow;
 }
 
 </style>
